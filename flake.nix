25ec1c76a367de1fb5fac5c234af219dc2d764c8
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-23 17:57:45 +0100

    [Fix] Added config to imports

--- flake.nix
+++ flake.nix
@@ -6,18 +6,13 @@
     outputs = { self, ... } @inputs: 
     let
         system = "x86_64-linux";
-        pkgs = import inputs.nixpkgs {
-            inherit system;
-            config = {
-                allowUnfree = true;
-            };
-        };
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
     in
     {     
-        nixosModules.default = ./nix/nixos.nix;
-        homeManagerModules.default = ./nix/home.nix;
-        devShells.${system}.default = import ./nix/devshells.nix { inherit pkgs; };
         packages.${system}.default = import ./nix/pkg.nix { inherit pkgs; };
         apps.${system}.default = { type = "app"; program = "${self.packages.${system}.default}/bin/webserver"; };
+        devShells.${system}.default = import ./nix/devshells.nix { inherit pkgs; };
+        nixosModules.default = ./nix/nixos.nix;
+        homeManagerModules.default = ./nix/home.nix;
     };
 }
