84ed89b7db3b0744258a1e1b875a596480f8ef18
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-23 15:57:55 +0100

    [Fix] Testing how to pass config

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
                 allowUnfree = true;
             };
         };
-        nixModules = import ./nix { inherit pkgs; };
+        nixModules = import ./nix { inherit pkgs; config = self.config; };
     in
     {     
         nixosModules.default = nixModules.modules.nixosModule;
