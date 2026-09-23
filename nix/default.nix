0f73dc1a9a6bcf47cd735b10975e2cbba2fc2841
Adam Dunmore <adamfdunmore@gmail.com>
2026-09-23 15:56:05 +0100

    [Fix] Added config to imports

--- nix/default.nix
+++ nix/default.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
     devShells = import ./devshells.nix { inherit pkgs; };
-    modules = import ./module.nix { inherit pkgs; };
+    modules = import ./module.nix { inherit pkgs; inherit config; };
     package = import ./pkg.nix { inherit pkgs; };
 }
